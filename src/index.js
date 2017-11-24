d21cbf67468e2ee2e6bf9aeb6fb3dbc31364c4b4
Amador Mateo Fernandez <amateofernandez@gmail.com>
2017-11-24 07:30:53 +0100

    App ok

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('Hola a todos');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
